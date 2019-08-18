77236c968a9c036cbbdacdf690b7a09e782bdb09
Julian Andrés <julian.acl@gmail.com>
2019-08-18 03:48:01 -0300

    Add SearchBox functionality

--- frontend/src/views/SearchBox.js
+++ frontend/src/views/SearchBox.js
@@ -1,5 +1,39 @@
 import React from 'react'
 
-export default function SearchBox() {
-  return <h1>SearchBox</h1>
+export default function SearchBox({ history }) {
+  const searchformInput = React.createRef()
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    const query = searchformInput.current.value
+
+    if (query !== '') {
+      history.push(`/items?search=${query}`)
+    }
+  }
+
+  return (
+    <div className="searchbox">
+      <div className="searchbox-contents">
+
+        <a href="/" className="meli-logo"><img src="" alt="Mercado Libre"/></a>
+
+        <div className="searchbox-form">
+          <form role="search" onSubmit={handleSubmit}>
+            <input
+              className="searchform-input"
+              id="searchbox-input"
+              aria-label="Escribe lo que quieres encontrar"
+              type="text"
+              placeholder="Nunca dejes de buscar"
+              ref={searchformInput}
+            />
+            <button className="searchform-button" type="submit" aria-label="Buscar"><img src="" alt="Buscar"/></button>
+          </form>
+        </div>
+
+      </div>
+    </div>
+  )
 }
